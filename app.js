2f74858927c203a2f3b5d3517c1670131d15b189
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-10-15 21:52:52 +0300

    update english page

--- app.js
+++ app.js
@@ -3,30 +3,24 @@ $( document ).ready(function() {
 	$.ajax({
 	  	url: "data.json",
 	    success: function(data){
-			console.log(data.articles)
 		    $.each(data.articles, function(key, value) {
-		    	// var templ_header = $('<p id="' + value.name + '" class="outer">' + value.translate + '</p>');
-		    	var templ_header = $('<div>')
+
+		    	var block = $('<div>')
 		    						.attr('id', value.name)
 		    						.addClass('outer');
+
 		    	$('<a>',{
 				    text: value.translate,
-				    title: 'Blah',
-				    href: 'dir/' + value.name,
+				    href: 'dir/' + value.name + '.html',
 				    click: function(){ console.log(value.name);}
-				}).appendTo('#' + value.name);
-		    	// var templ_info = $('<div id="' + value.name + '-tasks" class="inner">' + value.task + '</div>');
-		    	var templ_info = $('<div>')
-		    						.attr('id', value.name + '-tasks')
-		    						.addClass('inner')
-		    						.text(value.task);
-		    	// var templ_footer = $('<p class="teacher">' + value.teacher + '</p>');
-		    	var templ_footer = $('<p>')
-		    						.addClass('teacher')
-		    						.text(value.teacher);
-		    	$('body').append(templ_header)
-		    			.append(templ_info)
-		    			.append(templ_footer);
+				}).appendTo(block);
+
+				$('<p>',{
+				    class: 'teacher',
+				    text: value.teacher
+				}).appendTo(block);		
+
+		    	$('body').append(block)
 		    });
 	  	}
 	});
